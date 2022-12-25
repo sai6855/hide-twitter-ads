86187a9460daa06ed4e24afb58c429137c09adeb
sai6855 <60743144+sai6855@users.noreply.github.com>
2022-12-25 12:25:47 +0530

    patch

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,8 @@
 window.addEventListener("scroll", () => {
-  const elements = document.getElementsByTagName("span");
   if (!window.location.href.includes("twitter.com")) {
     return;
   }
+  const elements = document.getElementsByTagName("span");
   for (let i = 0; i < elements.length - 1; i++) {
     if (elements[i].innerText.toLowerCase().includes("promoted")) {
       if (elements[i].closest("article")) {
